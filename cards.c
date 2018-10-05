d3809ea372bc7a5e0ead5fa0cd982ddcb8d33212
Vinícius Melo <viniciusdesk@icloud.com>
2018-10-05 12:42:25 -0300

    Initial Repo, add 2 files in C language and one in binary

---
+++ cards.c
@@ -0,0 +1,33 @@
+/*
+ * Program to evaluate face values.
+ * Released under the Vegas Public License.
+ * (c)2014 The College Blackjack Team.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char card_name[3];
+    puts("Enter the card_name: ");
+    scanf("%2s", card_name);
+    int val = 0;
+    if (card_name[0] == 'K') {
+        val = 10;
+    }else if (card_name[0] == 'A'){
+        val = 11;
+    }else if (card_name[0] == 'J'){
+       val = 10;
+    }else if (card_name[0] == 'Q'){
+        val = 10;
+    }else{
+        val = atoi(card_name);
+    }
+    if (val >= 3 && val <= 6)
+        puts("Count has gone up");
+    /*Otherwise check if the card was 10, J, Q or K */
+    else if (val == 10)
+        puts("Count has gone down");
+    return 0;
+
+}
---
+++ teste.c
@@ -0,0 +1,13 @@
+/* This is a training program
+ * I will learn C language and test my skill
+ * On files like this one.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+
+
+{
+
