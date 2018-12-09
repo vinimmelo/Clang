e6926439bf426da16f72fb832b975c268caadae9
Vinícius Melo <viniciusdesk@icloud.com>
2018-12-09 13:05:53 -0200

    Just test an improved version of geo2json

--- HeadFirst/geo2json.c
+++ HeadFirst/geo2json.c
@@ -16,11 +16,11 @@ int main()
 	    started = 1;
 
 	if ((latitude < -90) || (latitude > 90)){
-	    printf("Invalid latitude: %f\n", latitude); 
+        fprintf(stderr, "Invalid latitude: %f\n", latitude);
 	    return 2;
 	}
 	else if ((longitude < -180) || (longitude > 180)){
-	    printf("Invalid longitude: %f\n", longitude);
+	    fprintf(stderr, "Invalid longitude: %f\n", longitude);
 	    return 2;
 	}
 	printf("{latitude: %f, longitude: %f, info: '%s'}", latitude, longitude, info);
@@ -28,4 +28,4 @@ int main()
     puts("\n]");
     return 0;
 
-} 
+}
