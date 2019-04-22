3ac4b3d0174aef488049c537768b094a7012886e
Vinícius Melo <viniciusdesk@icloud.com>
2019-04-21 22:31:11 -0300

    Addition of programa.c from Alura.

---
+++ Alura/programa.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  printf("*************************************\n");
+  printf("Bem vindo ao jogo da adivinhação!\n");
+  printf("*************************************\n");
+
+  int numero_secreto = 42;
+
+}
